339a36617543ebc4182134fa2a481fa1647c68b4
YANGUE <yangueissa@gmail.com>
2025-10-22 17:16:58 +0200

    Remove initialization code from app.py

--- Budget_App/Backend/app.py
+++ Budget_App/Backend/app.py
@@ -1,14 +1,1 @@
-from flask import Flask
-from flask_cors import CORS
-from routes import api
-from database import init_db
 
-app = Flask(__name__)
-CORS(app)
-app.register_blueprint(api, url_prefix="/api")
-
-# Initialisation de la base de données
-init_db()
-
-if __name__ == "__main__":
-    app.run(debug=True)
